4d9fea0858899de0cc856745af6a00d827f43dcb
lshifr <lshifr@gmail.com>
2016-08-18 17:58:25 +0300

    Added users and messages to db. Improved the model appearance in the admin

--- ngmail/models.py
+++ ngmail/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 
+def shorten_string(s, slen):
+    if len(s) > slen:
+        return s[:slen]+'...'
+    else:
+        return s
+
+
 class NGUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -11,9 +18,19 @@ class NGUser(models.Model):
     email = models.EmailField()
     avatar_url = models.CharField(max_length=200)
 
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name,)
+
 
 class NGMessage(models.Model):
     sender = models.ForeignKey(NGUser, related_name='sender')
     recipient = models.ForeignKey(NGUser, related_name='recipient')
     date_and_time = models.DateTimeField(default=timezone.now)
     text = models.TextField(null=True)
+
+    MESSAGE_NAME_DISPLAY_LENGTH = 70
+
+    def __str__(self):
+        res = '[' + shorten_string(self.sender.__str__(), 30) + ']'
+        res += '[' + shorten_string(self.text, self.MESSAGE_NAME_DISPLAY_LENGTH) + ']'
+        return res
